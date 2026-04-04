c2a0c546f3c687fc69382ef2b74fb716b4974ae3
Caiketadeus <caikecarioca@gmail.com>
2026-04-04 20:07:51 -0300

    aula16
    css, imagem, lista

--- aula16/primeirajanela1/src/main/java/com/restaurante1/MainApp.java
+++ aula16/primeirajanela1/src/main/java/com/restaurante1/MainApp.java
@@ -2,21 +2,27 @@ package com.restaurante1;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.AlertType;
+import javafx.scene.control.Alert;  
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import java.io.File;
+
 
 import java.io.IOException;
+import java.util.Arrays;
 
 
 public class MainApp extends Application {
@@ -25,8 +31,19 @@ public class MainApp extends Application {
     @Override
     public void start(Stage stage) {
         
+        Image logo = new Image(getClass().getResourceAsStream("/images/pet.png"));
+        ImageView visulogo = new ImageView(logo);
+        visulogo.setFitHeight(60);
+        visulogo.setPreserveRatio(true);
+
         Label lblTitulo = new Label("Cadastre seus pets na nossa veterinaria");
-        lblTitulo.setStyle("-fx-font-size: 24px; -fx-font-weight: bold; -fx-text-fill: darkred;");
+        lblTitulo.getStyleClass().add("titulo");
+        //lblTitulo.setStyle("-fx-font-size: 24px; -fx-font-weight: bold; -fx-text-fill: darkred;");
+
+        HBox header = new HBox(10);
+        header.getChildren().addAll(visulogo, lblTitulo);
+        header.getStyleClass().add("header");
+    
 
         Label lblNome = new Label("Nome do pet:");
         TextField txtNome = new TextField();
@@ -67,21 +84,31 @@ public class MainApp extends Application {
                     aviso.setContentText("Preencha todos os campos");
                     aviso.showAndWait();
                 }else{
-                    System.out.println("nome: "+ nome);
-                System.out.println("idade: "+ idade);
-                System.out.println("raca: "+ raca);
-                System.out.println("sexo: "+ sexo);
+                    if(!idade.matches("\\d*")){
 
-                cadastro.appendText("\nCadastro concluido...\n");
-                cadastro.appendText("nome: "+ nome+"\n");
-                cadastro.appendText("idade: "+ idade+"\n");
-                cadastro.appendText("raca: "+ raca+"\n");
-                cadastro.appendText("sexo: "+ sexo+"\n");
+                        Alert avisonum = new Alert(Alert.AlertType.WARNING);
+                        
+                        avisonum.setTitle("Erro");
+                        avisonum.setHeaderText("Erro de Preenchimento");
+                        avisonum.setContentText("Preencha o campo idade com numeros");
+                        avisonum.showAndWait();
+                    }else{
 
-                txtIdade.clear();
-                txtNome.clear();
-                txtRaca.clear();
-                
+                        System.out.println("nome: "+ nome);
+                        System.out.println("idade: "+ idade);
+                        System.out.println("raca: "+ raca);
+                        System.out.println("sexo: "+ sexo);
+
+                        cadastro.appendText("\nCadastro concluido...\n");
+                        cadastro.appendText("nome: "+ nome+"\n");
+                        cadastro.appendText("idade: "+ idade+"\n");
+                        cadastro.appendText("raca: "+ raca+"\n");
+                        cadastro.appendText("sexo: "+ sexo+"\n");
+
+                        txtIdade.clear();
+                        txtNome.clear();
+                        txtRaca.clear();
+                }
                 }
                 
         });
@@ -111,41 +138,42 @@ public class MainApp extends Application {
         HBox linhaRaca = new HBox(10);
         HBox linhaSexo = new HBox(10); 
         HBox linhaBtn = new HBox(10);
+        HBox linhaCadastro = new HBox(10);
 
         linhaNome.getChildren().addAll(lblNome, txtNome);
         linhaIdade.getChildren().addAll(lblIdade, txtIdade);
         linhaRaca.getChildren().addAll(lblRaca, txtRaca);
         linhaSexo.getChildren().addAll(lblSexo, cbmSexo);
         linhaBtn.getChildren().addAll(btnCadastrar, btnFinalizar);
+        linhaCadastro.getChildren().add(cadastro);
        
 
-        VBox vertical = new VBox(20);
+        VBox vertical = new VBox(30);
 
-        vertical.getChildren().addAll(lblTitulo, linhaNome, linhaIdade, linhaRaca, linhaSexo, linhaBtn, cadastro);
+        Arrays.asList(header, linhaNome, linhaIdade, linhaRaca, linhaSexo, linhaBtn, linhaCadastro)
+      .forEach(linha -> linha.setAlignment(Pos.CENTER));
+        
+        vertical.setAlignment(Pos.CENTER); 
+
+        vertical.getChildren().addAll(header, linhaNome, linhaIdade, linhaRaca, linhaSexo, linhaBtn, linhaCadastro);
+
+        //VBox.setVgrow(linhaCadastro, Priority.ALWAYS);
+        //HBox.setHgrow(linhaNome, Priority.ALWAYS);
+
+        //vertical.setMaxWidth(2000);
 
         Scene scene = new Scene(vertical, 1000, 600);
+
+        //vertical.prefWidthProperty().bind(scene.widthProperty());
+        //vertical.prefHeightProperty().bind(scene.heightProperty());
+
+        String css = getClass().getResource("/styles/style.css").toExternalForm();
+        scene.getStylesheets().add(css);
 
         stage.setTitle("Cadastrar Pet");
         stage.setScene(scene);
         stage.show();
     }
-
-    static void setRoot(String fxml) throws IOException {
-        setRoot(fxml,stage.getTitle());
-    }
-
-    static void setRoot(String fxml, String title) throws IOException {
-        Scene scene = new Scene(loadFXML(fxml));
-        stage.setTitle(title);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/fxml/"+fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
 
     public static void main(String[] args) {
         launch(args);
