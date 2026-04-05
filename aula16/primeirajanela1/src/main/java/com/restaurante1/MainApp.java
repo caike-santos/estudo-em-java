e42100bb70ee517f7dbe615be2e27d47761f0cf9
Caiketadeus <caikecarioca@gmail.com>
2026-04-04 22:05:00 -0300

    aula16
    pagina login

--- aula16/primeirajanela1/src/main/java/com/restaurante1/MainApp.java
+++ aula16/primeirajanela1/src/main/java/com/restaurante1/MainApp.java
@@ -1,9 +1,7 @@
 package com.restaurante1;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;  
 import javafx.scene.control.Button;
@@ -14,42 +12,100 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import java.io.File;
-
-
-import java.io.IOException;
+import javafx.scene.control.PasswordField;
 import java.util.Arrays;
 
 
 public class MainApp extends Application {
-    private static Stage stage;
+    
+    private Scene cenaLogin;
+    private Scene cenaCadastro;
+    
 
     @Override
     public void start(Stage stage) {
-        
-        Image logo = new Image(getClass().getResourceAsStream("/images/pet.png"));
+
+       //login
+
+       //header
+       Image logo = new Image(getClass().getResourceAsStream("/images/pet.png"));
         ImageView visulogo = new ImageView(logo);
         visulogo.setFitHeight(60);
         visulogo.setPreserveRatio(true);
 
-        Label lblTitulo = new Label("Cadastre seus pets na nossa veterinaria");
+        Label lblTitulo = new Label("Logue para se cadastrar");
         lblTitulo.getStyleClass().add("titulo");
         //lblTitulo.setStyle("-fx-font-size: 24px; -fx-font-weight: bold; -fx-text-fill: darkred;");
 
-        HBox header = new HBox(10);
-        header.getChildren().addAll(visulogo, lblTitulo);
-        header.getStyleClass().add("header");
-    
+        Button btnDeslogar = new Button("Deslogar");
 
+        btnDeslogar.setOnAction(evento -> {
+            stage.setTitle("Login");
+            stage.setScene(cenaLogin);
+            stage.show();
+        });
+        
+
+        HBox headerLogin = new HBox(10);
+        headerLogin.getChildren().addAll(visulogo, lblTitulo);
+        headerLogin.getStyleClass().add("header");
+        //fimHeader
+
+        //formularioLogin
+        Label lblusuario = new Label("Usuario");
+        TextField txtUsuario = new TextField();
+        txtUsuario.setMaxWidth(250); 
+       
+        Label lblsenha = new Label("Senha");
+        PasswordField txtSenha = new PasswordField();
+        txtSenha.setMaxWidth(250);
+
+        Button btnLogar = new Button("Entrar");
+        
+
+        HBox linhaUsuario = new HBox(10);
+        linhaUsuario.getChildren().addAll(lblusuario, txtUsuario);
+
+        HBox linhaSenha = new HBox(10);
+        linhaSenha.getChildren().addAll(lblsenha, txtSenha);
+
+        HBox linhaLogar = new HBox(10);
+        linhaLogar.getChildren().add(btnLogar);
+        //fimFormularioLogin
+
+        //montandoCena
+        VBox verticalLogin = new VBox(15);
+        verticalLogin.getChildren().addAll(headerLogin, linhaUsuario, linhaSenha, btnLogar);
+
+        verticalLogin.setAlignment(Pos.CENTER);
+
+        Arrays.asList(headerLogin, linhaUsuario, linhaSenha, linhaLogar).forEach(linha -> linha.setAlignment(Pos.CENTER));
+
+        cenaLogin = new Scene(verticalLogin, 1000, 600);
+        //fimMontandoCena
+        
+        //Cadastro
+
+        //header
+        ImageView visulogoCadastro = new ImageView(logo);
+        visulogoCadastro.setFitHeight(60);
+        visulogoCadastro.setPreserveRatio(true);
+
+        Label lblTituloCadastro = new Label("Cadastre seus pets na nossa veterinaria");
+        lblTituloCadastro.getStyleClass().add("titulo");
+
+        HBox header = new HBox(20);
+        header.getChildren().addAll(visulogoCadastro, lblTituloCadastro, btnDeslogar);
+        header.getStyleClass().add("header");
+        //fimHeader
+
+        //formulario
         Label lblNome = new Label("Nome do pet:");
         TextField txtNome = new TextField();
         txtNome.setMaxWidth(250);
         
-
         Label lblIdade = new Label("Idade do pet:");
         TextField txtIdade = new TextField();
         txtIdade.setMaxWidth(250);
@@ -132,7 +188,10 @@ public class MainApp extends Application {
                 cadastro.clear();
             }
         });
+        //fimCadastro
+        
 
+        //montandoCena
         HBox linhaNome = new HBox(10);
         HBox linhaIdade = new HBox(10);
         HBox linhaRaca = new HBox(10);
@@ -156,23 +215,48 @@ public class MainApp extends Application {
         vertical.setAlignment(Pos.CENTER); 
 
         vertical.getChildren().addAll(header, linhaNome, linhaIdade, linhaRaca, linhaSexo, linhaBtn, linhaCadastro);
+        
 
         //VBox.setVgrow(linhaCadastro, Priority.ALWAYS);
         //HBox.setHgrow(linhaNome, Priority.ALWAYS);
 
         //vertical.setMaxWidth(2000);
+         cenaCadastro = new Scene(vertical, 1000, 600);
+         String css = getClass().getResource("/styles/style.css").toExternalForm();
+        cenaCadastro.getStylesheets().add(css);
+        //fimMontandoCena
+        //fimCadastro
 
-        Scene scene = new Scene(vertical, 1000, 600);
+        btnLogar.setOnAction(evento -> {
+            
+            if(txtUsuario.getText().equals("caike") && txtSenha.getText().equals("123")){
+                
+                stage.setTitle("Cadastrar Pet");
+                stage.setScene(cenaCadastro);
+                stage.show();
 
-        //vertical.prefWidthProperty().bind(scene.widthProperty());
-        //vertical.prefHeightProperty().bind(scene.heightProperty());
+                //vertical.prefWidthProperty().bind(scene.widthProperty());
+                //vertical.prefHeightProperty().bind(scene.heightProperty());
+               
+            }else{
 
-        String css = getClass().getResource("/styles/style.css").toExternalForm();
-        scene.getStylesheets().add(css);
+                Alert erroLogin = new Alert(Alert.AlertType.WARNING);
+                erroLogin.setTitle("Erro");
+                erroLogin.setHeaderText("Usuario ou Senha invalidos");
+                erroLogin.setContentText("Digite o usuario e a senha corretamente");
+                erroLogin.showAndWait();
+            }
+        
+        });
 
-        stage.setTitle("Cadastrar Pet");
-        stage.setScene(scene);
+        String cssLogin = getClass().getResource("/styles/style.css").toExternalForm();
+        cenaLogin.getStylesheets().add(cssLogin);
+        stage.setTitle("Login");
+        stage.setScene(cenaLogin);
         stage.show();
+       //fimLogin
+
+       
     }
 
     public static void main(String[] args) {
